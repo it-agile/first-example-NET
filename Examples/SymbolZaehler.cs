35a335a4927dd4ae4bfdf47dd330b643e1c6b67f
Agile Developer ADS <csddev@it-agile.de>
2020-08-06 16:13:03 +0200

    Fragemnz zähler

---
+++ Examples/SymbolZaehler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace Examples
+{
+    class SymbolZaehler
+    {
+        public int ErmittleFragmente(TextReader datenStream)
+        {
+            var anzahl = 0;
+            var zeile = "";
+            while((zeile = datenStream.ReadLine()) != null)
+            {
+                string[] fragments = zeile.Split(new string [] { "/>" }, StringSplitOptions.RemoveEmptyEntries);
+                if(fragments.Length > 1)
+                {
+                    anzahl += fragments.Length - 1;
+                }
+            }
+            datenStream.Close();
+            return anzahl;
+        }
+    }
+}
---
+++ Examples/SymbolZaehlerTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+using System.IO;
+
+namespace Examples
+{
+    [TestClass]
+    public class SymbolZaehlerTest
+    {
+        [TestMethod]
+        public void ZaehltSymbole()
+        {
+            WebClient client = new WebClient();
+            string data = client.DownloadString("http://agileinaflash.blogspot.com/feeds/posts/default");
+            TextReader datenStream = new StringReader(data);
+
+            SymbolZaehler zaehlerUnterTest = new SymbolZaehler();
+            Assert.AreEqual(475, zaehlerUnterTest.ErmittleFragmente(datenStream));
+        }
+
+        [TestMethod]
+        public void ZaehltSymboleVonVielenZeilen()
+        {
+            WebClient client = new WebClient();
+            string data = client.DownloadString("https://www.it-agile.de/impressum/");
+            TextReader datenStream = new StringReader(data);
+
+            SymbolZaehler zaehlerUnterTest = new SymbolZaehler();
+            Assert.AreEqual(9, zaehlerUnterTest.ErmittleFragmente(datenStream));
+        }
+    }
+}
