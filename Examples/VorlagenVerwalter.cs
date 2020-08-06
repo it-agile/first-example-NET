de7b3fc26830fec6c36c5093d62704112c35371a
Agile Developer ADS <csddev@it-agile.de>
2020-08-06 14:49:56 +0200

    Add Vorlagen Test

---
+++ Examples/VorlagenVerwalter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Examples
+{
+    internal class VorlagenVerwalter
+    {
+        private Dictionary<string, string> _vorlagen;
+
+        public VorlagenVerwalter()
+        {
+            _vorlagen = new Dictionary<string, string>();
+        }
+
+        internal void definiere(string n, string t)
+        {
+            _vorlagen.Add(n, t);
+        }
+
+        internal object FuelleAus(string n, Dictionary<string, object> werte)
+        {
+            var template = _vorlagen[n];
+            var result = template;
+
+            foreach (var item in werte)
+            {
+                result = result.Replace("{" + item.Key + "}", item.Value.ToString());
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Examples/VorlagenVerwalterTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Examples
+{
+    [TestClass]
+    public class VorlagenVerwalterTest
+    {
+        [TestMethod]
+        public void ErsetzePlatzhalterMitRichtigenWerten()
+        {
+            VorlagenVerwalter vorlagen = new VorlagenVerwalter();
+            vorlagen.definiere("foo", "foo {bar}");
+            var werte = new Dictionary<string, object>();
+            werte.Add("bar", "baz");
+            var result = vorlagen.FuelleAus("foo", werte);
+            Assert.AreEqual("foo baz", result);
+        }
+    }
+}
