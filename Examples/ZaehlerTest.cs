5a2bc5b2fcae9a4532e0895b99c1d4c1336bf2a9
Agile Developer ADS <csddev@it-agile.de>
2020-08-06 14:56:15 +0200

    Added static Zaehler Test

---
+++ Examples/ZaehlerTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Examples
+{
+    [TestClass]
+    public class ZaehlerTest
+    {
+        [TestMethod]
+        public void IstNullBeiStart()
+        {
+            Assert.AreEqual(0, Zaehler.Stand);
+        }
+
+        [TestMethod]
+        public void KannErhoertWerden()
+        {
+            Zaehler.Erhoehe();
+            Assert.AreEqual(1, Zaehler.Stand);
+        }
+
+        [TestMethod]
+        public void KannNochmalErhoertWerden()
+        {
+            Zaehler.Erhoehe();
+            Assert.AreEqual(2, Zaehler.Stand);
+        }
+    }
+}
